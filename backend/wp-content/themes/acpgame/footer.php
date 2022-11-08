95ce459e5fca356aba06b29eb4339ebdfc672484
iliagromov <ilia.workproffile@gmail.com>
2022-11-08 14:44:13 +0300

    Настроил шаблоны

---
+++ backend/wp-content/themes/acpgame/footer.php
@@ -0,0 +1,55 @@
+<footer class="footer">
+			<div class="wrapper">
+				<div class="footer-block">
+					<div class="footer-block__col footer-block__img"><a href="/">
+							<div class="page__img"><img class="page_desktop" src="assets/img/logo-small.png"><img class="page_mobile" src="assets/img/logo-big.png"></div></a></div>
+					<div class="footer-block__col footer-block__about"><a class="page__link" href="#">О нас</a><a class="page__link" href="#">Команда</a><a class="page__link" href="#">Фотоотчеты</a><a class="page__link" href="#">Контакты</a><a class="page__link" href="#">СМИ о нас</a></div>
+					<div class="footer-block__col footer-block__corporation"><a class="page__link" href="#">Корпоративные игры</a><a class="page__link" href="#">Битва корпораций</a></div>
+					<div class="footer-block__col footer-block__games"><a class="page__link" href="#">Формат игр</a><a class="page__link" href="#">Угадать за 60 секунд</a><a class="page__link" href="#">Читай.Слушай.Смотри.</a><a class="page__link" href="#">Детские игры</a></div>
+					<div class="footer-block__col footer-block__phones"><a class="page__link" href="#">8 (495) 941-83-25</a><a class="page__link" href="#">8 (985) 774-32-40</a></div>
+					<div class="footer-block__col footer-block__soical"><a class="page__link" href="#">Facebook</a><a class="page__link" href="#">Instagram</a><a class="page__link" href="#">Video</a></div>
+				</div>
+				<div class="footer__copy">© ACP GAME, 2012-2020</div>
+				<div class="author"> <span>разработано в </span>
+					<div> <a class="page__link" href="https://iliagromov.github.io/">gromov.studio</a></div>
+				</div>
+			</div>
+		</footer>
+		<section class="modal modal-menu page_hidden" id="modal-menu">
+			<div class="modal-container">
+				<div class="modal__action">
+					<button class="page__btn page__btn_close close-modal" data-modal="modal-menu"></button>
+				</div>
+				<div class="modal__content">
+					<div class="modal__content-nav">
+						<div class="page__img"><a href="../"><img src="assets/img/logo-big.png" alt=""></a></div>
+						<nav class="page__nav"><a class="page__link" href="about/about.html">О нас</a><a class="page__link" href="team/team.html">Команда</a><a class="page__link" href="games-of-corparations/games-of-corparations.html">Корпоративные игры</a><a class="page__link" href="games-format/games-format.html">Формат игр</a>
+							<ul class="page__lists">
+								<li class="page__list"> <a class="page__link" href="games-format/games-format.html">Угадать за 60 секунд</a></li>
+								<li class="page__list"> <a class="page__link" href="games-format/games-format.html">Читай.Слушай.Смотри.</a></li>
+								<li class="page__list"> <a class="page__link" href="games-format/games-format.html">Детские игры</a></li>
+							</ul><a class="page__link" href="battle-of-corporations/battle-of-corporations.html">Битва корпораций</a><a class="page__link" href="gallery-page/gallery-page.html">Фотоотчеты</a><a class="page__link" href="video-page/video-page.html">Видео</a><a class="page__link" href="smi/smi.html">СМИ о нас</a><a class="page__link" href="contacts/contacts.html">Контакты</a>
+						</nav>
+						<div class="page__links"><a class="page__link" href="tel:+7 (495) 941-83-25">8 (495) 941-83-25</a><a class="page__link" href="tel:+7 (985) 774-32-40">8 (985) 774-32-40</a></div>
+						<div class="social">
+							<button class="page__btn page__icon page__icon_facebook_white"></button>
+							<button class="page__btn page__icon page__icon_instagram_white"></button>
+						</div>
+					</div>
+					<div class="modal__content-info">
+						<div class="page__img"><img src="assets/img/img-modalmenu-1.png" alt=""></div>
+						<div class="page__img"><img src="assets/img/img-modalmenu-2.png" alt=""></div>
+						<div class="page__img"><img src="assets/img/img-modalmenu-3.png" alt=""></div>
+						<div class="page__img"><img src="assets/img/img-modalmenu-4.png" alt=""></div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<script src="libs/js/tiny-slider.js"></script>
+		<script src="js/debug-grid-create.js"></script>
+		<script src="js/modal.js"></script>
+		<script src="js/store-scroll.js"></script>
+		<script src="js/main.js"></script>
+    <?php wp_footer(); ?>
+	</body>
+</html>
